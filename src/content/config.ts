45dc3d2b24dbae39177750502024958dcc661716
madcampos <madcampos@outlook.com>
2023-12-15 13:11:16 -0500

    feat: update image reference on frontmatter

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,10 +2,10 @@ import { defineCollection, z as zod } from 'astro:content';
 
 const blogCollection = defineCollection({
 	type: 'content',
-	schema: zod.object({
+	schema: ({ image }) => zod.object({
 		title: zod.string().describe('The post title that will be displayed as the top of the post page or as the heading on lists.'),
 		summary: zod.string().describe('A summary for the post that will show in list pages.'),
-		image: zod.string().optional().describe('The post main "hero" image path, relative to the blog root.'),
+		image: image().optional().describe('The post main "hero" image path, relative to the blog root.'),
 		imageAlt: zod.string().optional().describe('The post main image alt text.'),
 		createdAt: zod.string().transform((date: string) => new Date(date)).describe('The post creation date.'),
 		updatedAt: zod.string().transform((date: string) => new Date(date)).optional().describe('The post last update date.'),
@@ -13,6 +13,13 @@ const blogCollection = defineCollection({
 		updates: zod.array(zod.string()).optional().describe('The post updates.')
 	})
 });
+
+export interface ImageProps {
+	src: string,
+	width: number,
+	height: number,
+	format: 'png' | 'jpg' | 'jpeg' | 'tiff' | 'webp' | 'gif' | 'svg' | 'avif'
+}
 
 export const collections = {
 	blog: blogCollection
